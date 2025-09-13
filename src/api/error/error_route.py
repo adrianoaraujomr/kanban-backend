a744f19edb47161d877370994fd5c6da6d78cd91
Adriano <adrianoaraujomr@gmail.com>
2025-09-13 14:54:22 -0300

    Removido rota de dar raise em erro

--- src/api/error/error_route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from src.global_error_handling import InsufficientStorage
-
-error_route = Blueprint("error_route", __name__)
-
-@error_route.route("/insufficient-storage", strict_slashes=False)
-def insufficient_storage():
-    raise InsufficientStorage()
--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ from src.database import create_db
 from src.api.usuario.usuario_route import usuario_route
 from src.api.to_do_card.to_do_card_route import to_do_card_route
 from src.api.math.math_route import math_route
-from src.api.error.error_route import error_route
 from src.global_error_handling import handle_exception_http_exception, handle_exception_sqlalchemy_error
 
 def create_app():
@@ -18,7 +17,6 @@ def create_app():
     app.register_blueprint(usuario_route, url_prefix="/usuario")
     app.register_blueprint(to_do_card_route, url_prefix="/to-do-card")
     app.register_blueprint(math_route, url_prefix="/math")
-    app.register_blueprint(error_route, url_prefix="/error")
 
     app.register_error_handler(HTTPException, handle_exception_http_exception)
     app.register_error_handler(SQLAlchemyError, handle_exception_sqlalchemy_error)
